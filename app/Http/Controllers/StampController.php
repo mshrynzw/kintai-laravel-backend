97b5144a867cc1257cfa22a7694db432fe69b961
mshrynzw <iq87io25@gmail.com>
2025-04-10 15:59:36 +0900

    Edit Stamp

--- app/Http/Controllers/StampController.php
+++ app/Http/Controllers/StampController.php
@@ -7,10 +7,23 @@ use Illuminate\Http\Request;
 
 class StampController extends Controller
 {
+    public function index(Request $request)
+    {
+        $query = Stamp::where('user_id', 1);
+
+        if ($request->has('date')) {
+            $query->whereDate('stamped_at', $request->date);
+        }
+
+        $stamp = $query->orderBy('stamped_at', 'desc')->first();
+
+        return response()->json($stamp);
+    }
+
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'type' => ['required', 'in:clock_in,break_start,break_end,clock_out'],
+            'type' => ['required', 'in:clock_in,break_in,break_out,clock_out'],
             'stamped_at' => ['required', 'date']
         ]);
 
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\StampController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('stamps', [StampController::class, 'store']);
+Route::get('stamps', [StampController::class, 'index']);
